a976f4802546da77484cc75ea999ef66f06f3b24
giorgiabes <44198003+giorgiabes@users.noreply.github.com>
2025-06-04 16:44:31 +0400

    add report

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from stats import get_num_words, character_count
+from stats import get_num_words, sorted_lst
 
 
 def main():
 
-    print(get_num_words())
-    print(character_count())
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    num_words = get_num_words()
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    sorted_lst()
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,18 @@
+def sort_on(dict):
+    return dict["num"]
+
+
+def sorted_lst():
+    lst = []
+    for char, num in character_count().items():
+        if char.isalpha():
+            lst.append({"char": char, "num": num})
+
+    lst.sort(reverse=True, key=sort_on)
+    for i in lst:
+        print(f"{i["char"]}: {i["num"]}")
+
+
 def character_count():
     text = get_text("books/frankenstein.txt").lower()
     my_dict = {}
@@ -12,7 +27,7 @@ def character_count():
 def get_num_words():
     text = get_text("books/frankenstein.txt")
     num_words = len(text.split())
-    return f"{num_words} words found in the document"
+    return num_words
 
 
 def get_text(filepath):
